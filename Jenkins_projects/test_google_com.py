d9cec70eb702609a476adc2d9dedd2a9619b24ba
manny121 <manny121@gmail.com>
2022-10-26 11:40:54 -0400

    making changes to dev

---
+++ Jenkins_projects/test_google_com.py
@@ -0,0 +1,52 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+# class TestGoogle():
+
+# @pytest.fixture(scope="class")
+# def browser():
+#     driver = webdriver.Chrome(ChromeDriverManager().install())
+#     driver.implicitly_wait(10)
+#
+#     yield driver
+#     driver.quit()
+#
+# def test_google_site(browser):
+#     try:
+#         driver=browser
+#         driver.get("https://www.google.com")
+#         driver.find_element(By.NAME, "q").send_keys("jenkins")
+#         element = driver.find_element(By.NAME, "btnK")
+#         element.click()
+#         assert "Google" == driver.title
+#
+#     except Exception as e:
+#         print(e)
+#     finally:
+#         driver.close()
+
+
+@pytest.fixture(scope="class")
+def browser():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.implicitly_wait(10)
+
+    yield driver
+    driver.quit()
+
+def test_google_site(browser):
+    try:
+
+        browser.get("https://www.google.com")
+        browser.find_element(By.NAME, "q").send_keys("jenkins")
+        element = browser.find_element(By.NAME, "btnK")
+        element.click()
+        assert "Google" == browser.title
+
+    except Exception as e:
+        print(e)
+    finally:
+        browser.close()
